ab5b934d1b7971fcdc1d90524a2858fb20cc0ea7
danilasoba <danilasoba@gmail.com>
2023-10-02 21:49:56 +0300

    Third commit (added Button1)

--- app/src/main/java/com/example/laba1/MainActivity.java
+++ app/src/main/java/com/example/laba1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.laba1;
 
+import android.graphics.Color;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void butClick (View view){
+        TextView txtHello = findViewById(R.id.textView);
+        txtHello.setText("Postavte 10");
     }
 }
